750630b7029a654ac7bd8819e9faf4bb83e8c8d8
Aditya <akdevil623@gmail.com>
2021-05-24 00:39:32 +0530

    basic testing of app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,26 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { of } from 'rxjs';
 import { AppComponent } from './app.component';
+import { FetchRepoService } from './fetch-repo-service.service';
+import {delay} from 'rxjs/operators';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule  
       ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        FetchRepoService,
+        NgxSpinnerService
+      ]
     }).compileComponents();
   });
 
@@ -26,10 +36,31 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('GithubRepositories');
   });
 
-  it('should render title', () => {
+  it('should return user git details',fakeAsync(()=>{
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('GithubRepositories app is running!');
-  });
+    let component = fixture.componentInstance;
+    component.userName = 'aditya-kr';
+    let fetchRepoService = fixture.debugElement.componentInstance.injector.get(FetchRepoService);
+    let stub = spyOn(fetchRepoService,"getUserViaUserName").and.callFake(() =>{
+      return of({name: 'Tony Stark',
+        bio: 'iron man',
+        avatarUrl: 'you already know it',
+        twitterHandle: 'i_heart_ironman',
+        location: 'New York',
+        gitUrl : 'private repo baby'}).pipe(delay(300));
+    });
+    component.searchUserName();
+    tick(300);
+
+    expect(component.userData).toEqual({name: 'Tony Stark',
+    bio: 'iron man',
+    avatarUrl: 'you already know it',
+    twitterHandle: 'i_heart_ironman',
+    location: 'New York',
+    gitUrl : 'private repo baby'});
+
+    expect(component.isAValidUser).toEqual(true);
+
+
+  }))
 });
